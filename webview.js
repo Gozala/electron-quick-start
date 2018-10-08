6bd059bbffe87f02c4f4077e7a36bd10d6824ea2
Irakli Gozalishvili <rfobic@gmail.com>
2018-10-08 13:08:52 -0700

    Make code little less hacky

--- webview.js
+++ webview.js
@@ -1,7 +1,20 @@
 const handler = {
   handleEvent(event) {
-    if (event.target.id === "open-button") {
-      let host = window.open("./host.html", "modal")
+    switch (event.target.id) {
+      case "open-button": {
+        window.host = window.open("./host.html", "tab")
+        document.querySelector("#log").textContent += `\nOpened window: ${
+          window.host
+        }`
+        return
+      }
+      case "update-host": {
+        host.document.body.innerHTML = "<h1>Updated doc</h1>"
+        document.querySelector("#log").textContent += `\nUpdated host doc: ${
+          host.document.body.innerHTML
+        }`
+        return
+      }
     }
   }
 }
